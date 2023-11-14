3fbee4250c59ed2548586328a1d94aaaa014d104
Ha Duy Long <longha1032002@gmail.com>
2023-11-14 23:36:43 +0700

    chuc nang manage members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -77,6 +77,7 @@ export const ServerHeader = ({
 
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("members", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Manage members
