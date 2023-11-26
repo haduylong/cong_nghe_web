92c4fa3e0d5cd16dc956476e1a256d1b4b42d377
Ha Duy Long <longha1032002@gmail.com>
2023-11-26 15:42:05 +0700

    create chat-messages component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
 
 import { cn } from '@/lib/utils'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,7 +37,9 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
